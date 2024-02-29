9be5b83740a26b00840ae5b6acf104d315231261
spideryzarc <spideryzarc@gmail.com>
2024-02-29 17:53:15 -0300

    Add Facility Location Problem class and initialize variables

---
+++ codes/flp.py
@@ -0,0 +1,21 @@
+#Facility Location Problem
+
+class FLP:
+    def __init__(self):
+        self.demand = []
+        self.supply = []
+        self.cost = []
+        self.opening_cost = []
+        self.x = []
+        self.y = []
+        
+damand = [80, 270, 250, 160, 180]
+supply = [500, 900, 200, 300]
+cost = [[15, 20, 25, 30],
+        [10, 17, 14, 20],
+        [25, 10, 15, 10],
+        [30, 25, 20, 25],
+        [14, 12, 18, 16]]
+opening_cost = [1200, 1500, 1000, 1800]
+
+
